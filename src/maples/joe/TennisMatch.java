a7b0a83d1454503b450a7703fdf9b389145e774b
Joe Maples <joe@frap129.org>
2018-04-18 14:12:55 -0400

    TennisMatch: Slight rework
    Separate dates, only process winner on instantiation
    
    Signed-off-by: Joe Maples <joe@frap129.org>

--- src/maples/joe/TennisMatch.java
+++ src/maples/joe/TennisMatch.java
@@ -1,58 +1,50 @@
 package maples.joe;
 
 public class TennisMatch {
-    private int date;
-    private String playerOne;
-    private String playerTwo;
-    private String location;
-    private String scores;
+    private int dateYear, dateMonth, dateDay;
+    private String player1Id, player2Id, tournament, scores, winner;
 
-    public TennisMatch(int date, String playerOne, String playerTwo, String location, String scores) {
-        this.date = date;
-        this.playerOne = playerOne;
-        this.playerTwo = playerTwo;
-        this.location = location;
+    public TennisMatch(int dateYear, int dateMonth, int dateDay, String player1Id, String player2Id, String tournament, String scores) {
+        this.dateYear = dateYear;
+        this.dateMonth = dateMonth;
+        this.dateDay = dateDay;
+        this.player1Id = player1Id;
+        this.player2Id = player2Id;
+        this.tournament = tournament;
         this.scores = scores;
+        this.winner = determineWinner();
     }
 
-    public int getDate() {
-        return date;
+    public int getDateYear() {
+        return dateYear;
     }
 
-    public void setDate(int date) {
-        this.date = date;
+    public int getDateMonth() {
+        return dateMonth;
     }
 
-    public String getPlayerOne() {
-        return playerOne;
+    public int getDateDay() {
+        return dateDay;
     }
 
-    public void setPlayerOne(String playerOne) {
-        this.playerOne = playerOne;
+    public String getPlayer1Id() {
+        return player1Id;
     }
 
-    public String getPlayerTwo() {
-        return playerTwo;
+    public String getPlayer2Id() {
+        return player2Id;
     }
 
-    public void setPlayerTwo(String playerTwo) {
-        this.playerTwo = playerTwo;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
+    public String getTournament() {
+        return tournament;
     }
 
     public String getScores() {
         return scores;
     }
 
-    public void setScores(String scores) {
-        this.scores = scores;
+    public String getWinner() {
+        return winner;
     }
 
     private static boolean isInteger(String string) {
@@ -67,7 +59,7 @@ public class TennisMatch {
         return isValidInteger;
     }
 
-    private int evalScore(String setScore, int first, int last) {
+    private int evalSets(String setScore, int first, int last) {
         if (last < first)
             return Integer.MAX_VALUE;
 
@@ -76,30 +68,30 @@ public class TennisMatch {
                 return Integer.parseInt(setScore);
 
             int score = Integer.parseInt(setScore);
-            return score - evalScore(setScore, first + String.valueOf(score).length(), last) > 0 ? 1 : -1;
+            return score - evalSets(setScore, first + String.valueOf(score).length(), last) > 0 ? 1 : -1;
         }
 
-        return evalScore(setScore, ++first, last);
+        return evalSets(setScore, ++first, last);
     }
 
-    private int parseWins(String[] scoreArray, int first, int last){
+    private int parseGame(String[] scoreArray, int first, int last){
         if (last < first)
             return Integer.MAX_VALUE;
 
         if (first == last)
-            return evalScore(scoreArray[first], 0, 2);
+            return evalSets(scoreArray[first], 0, 2);
 
-        return evalScore(scoreArray[first], 0, 2) + parseWins(scoreArray, ++first, last);
+        return evalSets(scoreArray[first], 0, 2) + parseGame(scoreArray, ++first, last);
     }
 
-    public String getWinner() {
+    public String determineWinner() {
         String[] scoreArray = scores.split(",");
         int numMatches = scoreArray.length;
-        int playerOneWins = parseWins(scoreArray, 0, numMatches);
+        int playerOneWins = parseGame(scoreArray, 0, numMatches);
         if (playerOneWins > 0)
-            return playerOne;
+            return player1Id;
         else if (playerOneWins < 0)
-            return playerTwo;
+            return player1Id;
         else {
             System.out.println("Warning: Match ended in a tie. Check your input file for errors.");
             return "tie";
